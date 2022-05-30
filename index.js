da0556905e4b139105001e82aa803c34195ad06c
Ineb01 <benjamin.bachmayr@gmail.com>
2022-05-30 13:03:07 +0200

    First discord test

--- index.js
+++ index.js
@@ -1,14 +1,57 @@
+require('dotenv').config();
+
 const express = require('express')
 const path = require('path')
 const irish = require('./irish');
 const PORT = process.env.PORT || 5000
 
-const app = express();
+const { Routes } = require('discord-api-types/v9');
 
-app.get('/api/count', (req, res) => {
-  irish.countAvailableTables(8, new Date("2022-06-01")).then((data) => {
-    res.json({open:data});
+const { Client, Intents } = require('discord.js');
+
+
+//const app = express();
+
+/*app.get('/api/count', (req, res) => {
+  date = new Date()
+  while (date.getDay() != 6) {
+    date.setDate(date.getDate() + 1)
+  }
+  irish.countAvailableTables(8, date).then((data) => {
+    res.json({open:data, date:date.toDateString()});
   })
 })
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+*/
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+
+client.on("messageCreate", (message) => {
+  if (message.content.startsWith("ping")) {
+    console.log(message.author);
+    message.channel.send("pong!");
+  }
+});
+
+irish.ev.on("change", (data) => {
+  client.users.cache.get("304699475757236224").send(`${data.number} tables available on ${data.date.toDateString()}`);
+  console.log(data);
+});
+
+irish.ev.on("update", (data) => {
+  console.log("update:" + JSON.stringify(data));
+});
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.users.fetch("304699475757236224");
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+
--- irish.js
+++ irish.js
@@ -1,8 +1,13 @@
 var https = require('follow-redirects').https;
 var fs = require('fs');
-
+var events = require('events');
 
 let irish = {}
+
+irish.ev = new events.EventEmitter();
+
+irish.map = {}
+
 
 irish.url = "https://reservation.dish.co/rest/v1/hydra/establishment/253065/slots"
 irish.countAvailableTables = async (seats, date) => {
@@ -23,6 +28,25 @@ irish.countAvailableTables = async (seats, date) => {
         req.end();
     });
 }
+
+update = async()=>{
+    
+    let newmap = {};
+    for (let i=0;i<7;i++) {
+        date = new Date();
+        date.setDate(date.getDate() + i);
+        newmap[date.toISOString().slice(0, 10)] = await irish.countAvailableTables(8, date);
+    }
+    for (let key in newmap) {
+        if(newmap[key] != irish.map[key]) {
+            irish.ev.emit("change", {date:new Date(key), number:newmap[key]});
+        }
+    }
+    irish.map = newmap;
+    irish.ev.emit("update", irish.map);
+}
+
+setInterval(update, 1000)
 
 
 
